43cf3582ab8c5d7536a2e36f593b20ce2e575d69
viktoria-kan <viktoria1418@gmail.com>
2023-02-19 22:55:27 +0300

    Update my first file.py
    Add def summation and delete list

--- my first file.py
+++ my first file.py
@@ -1,3 +1,24 @@
-new_list = [5, 3.8, "data"]
-new_list.append("this is a string")
-print(new_list)
+from typing import Union
+
+
+def summation(x, y: Union[int, float]) -> Union[int, float]:
+    """
+    сложение двух чисел
+    :param x: int /  float
+    :param y: int /  float
+    :return:
+        сумма двух чисел
+    """
+
+    try:
+        int(x + y)
+        print(x + y)
+    except (TypeError, ValueError):
+        print("Ошибка! Введите данные в числовом формате (int/float)")
+    return x + y
+
+
+summation(['a', 'b'], ['c', 'd'])
+summation('a', 'd')
+summation(1.7, 2.3)
+summation(3, 4)
